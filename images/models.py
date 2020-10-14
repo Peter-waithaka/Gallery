3b64b03052c0f0d3daa553e168b2ed823ca2f85c
Peter-waithaka <peterwaithaka120@gmail.com>
2020-10-14 16:19:04 +0300

    models.images

--- images/models.py
+++ images/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Images (model.Model):
+    title = models.CharField(max_length=30)
+    description= models.CharField (max_length=30)

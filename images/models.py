6c9a8d9020bab26005bd273002360802c9457190
Peter-waithaka <peterwaithaka120@gmail.com>
2020-10-14 18:45:49 +0300

    bootstrap

--- images/models.py
+++ images/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Images (model.Model):
+class Images (models.Model):
     title = models.CharField(max_length=30)
     description= models.CharField (max_length=30)
+    image =models.ImageField('/images')
